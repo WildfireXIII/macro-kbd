def8a9cb511a64de8d28c351d7d9fd47e4c31e42
Nathan Martindale <nmblenderdude0@gmail.com>
2017-12-20 15:45:34 -0600

    Finally, it's something

--- inputs.py
+++ inputs.py
@@ -5,26 +5,24 @@ import keyboard
 
 from evdev import UInput, ecodes as e
 
-
+ui = UInput()
 
 listenmode = False # caps lock pressed, wait for a for sequence start
 commandmode = False
 
 
+def grab():
+    try:
+        device.grab()
+        print("GRABBED")
+    except: pass
 
+def ungrab():
+    try:
+        device.ungrab()
+        print("UNGRABBED")
+    except: pass
 
-
-
-
-#gui.keyDown('caps')
-#gui.keyDown('f')
-#gui.keyUp('f')
-
-
-#gui.hotkey('caps', 'f')
-
-
-ui = UInput()
 
 device = evdev.InputDevice('/dev/input/event0')
 print(device)
@@ -35,60 +33,69 @@ for event in device.read_loop():
         ke = evdev.KeyEvent(event)
         print(ke.keycode, ke.keystate)
 
+
+
+        if not commandmode:
+            keys = device.active_keys(True)
+            names = [thing[0] for thing in keys]
+
+            if 'KEY_LEFTMETA' in names and 'KEY_RIGHTCTRL' in names:
+                print(".........")
+                commandmode = True
+                ui.write_event(ke)
+                ui.write(e.EV_KEY, e.KEY_RIGHTCTRL, 0)
+                ui.write(e.EV_KEY, e.KEY_LEFTMETA, 0)
+                ui.syn()
+                grab()
+                
+        else:
+            if ke.keycode == 'KEY_ENTER' and ke.keystate == 0:
+                ungrab()
+                ui.write(e.EV_KEY, e.KEY_RIGHTCTRL, 1)
+                ui.write(e.EV_KEY, e.KEY_LEFTMETA, 1)
+                ui.write(e.EV_KEY, e.KEY_RIGHTCTRL, 0)
+                ui.write(e.EV_KEY, e.KEY_LEFTMETA, 0)
+                ui.syn()
+                commandmode = False
+            
+
+
+
+
+
+        
+'''
+
         if not listenmode and not commandmode: # wait for caps lock
             if ke.keycode == 'KEY_CAPSLOCK' and ke.keystate == 1:
                 listenmode = True
+
+
+                
         elif listenmode: # caps lock is down
             
             # if a is pressed (sequence to enter command mode)
-            if ke.keycode == 'KEY_A' and ke.keystate == 1:
-                ui.write_event(ke)
+            if ke.keycode == 'KEY_ESC' and ke.keystate == 1:
+                ui.write(e.EV_KEY, e.KEY_ESC, 1)
+                ui.write(e.EV_KEY, e.KEY_ESC, 0)
                 ui.write(e.EV_KEY, e.KEY_CAPSLOCK, 0)
-                ui.write(e.EV_KEY, e.KEY_A, 0)
                 ui.syn()
+                
+                grab()
                 
                 commandmode = True
-                print("COMMAND MODE")
 
-                try: 
-                    device.grab()
-                    print("grabbed")
-                except: pass
             elif ke.keystate != 2: # ignore held down keys
                 listenmode = False
+
+                
         elif commandmode:
             if ke.keycode == 'KEY_ENTER' and ke.keystate == 0:
-                try: 
-                    device.ungrab()
-                    print("ungrabbed")
-                except: pass
+                ungrab()
+
+
+                
                 commandmode = False
-                ui.write(e.EV_KEY, e.KEY_CAPSLOCK, 1)
-                ui.write(e.EV_KEY, e.KEY_CAPSLOCK, 0)
-                ui.syn()
-
-
+'''
                 
-                #ui.write(e.EV_KEY, e.KEY_F, 0)
-                #ui.write_event(ke)
-                
-                
-        
-
-        #if ke.keycode == 'KEY_A' and ke.keystate == 1:
-            #subprocess.Popen(['python', '/home/dwl/lab/MacroKbd/actuators.py'])
-            #try: device.grab()
-            #except: print(" - Failed to grab")
-
-            #ui.write(e.EV_KEY, e.KEY_A, 0)
-            #ui.syn()
-            
-            
-        #if ke.keycode == 'KEY_B' and ke.keystate == 0:
-            #try: device.ungrab()
-            #except: print(" - Failed to grab")
-
-
-        #if (evdev.ecodes.
-
 ui.close()

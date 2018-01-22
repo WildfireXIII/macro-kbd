50ba57406f48afc2783477a4de5c26ddba465e78
Nathan Martindale <nmblenderdude0@gmail.com>
2018-01-22 12:20:05 -0600

    Progress on config file

--- inputs.py
+++ inputs.py
@@ -2,6 +2,8 @@ import pyautogui as gui
 import evdev
 import subprocess
 import keyboard
+import sys
+import json
 
 from evdev import UInput, ecodes as e
 
@@ -11,10 +13,17 @@ listenmode = False # caps lock pressed, wait for a for sequence start
 commandmode = False
 
 
+# read in config
+settings = {}
 
-
-
-
+if len(sys.argv) >= 2:
+    config_file = sys.argv[1]
+    with open(config_file) as json_data_file:
+        settings = json.load(json_data_file)
+else:
+    # assign setting defaults
+    settings = { "device_sel_mode"
+    pass
 
 
 
@@ -39,6 +48,17 @@ def enter():
     ui.write(e.EV_KEY, e.KEY_ENTER, 1)
     ui.write(e.EV_KEY, e.KEY_ENTER, 0)
     ui.syn()
+
+
+def listDevices():
+    devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+    for device in devices:
+        print(device.fn, device.name, device.phys) 
+
+
+# display devices and offer choice to user
+listDevices()
+
 
 
 device = evdev.InputDevice('/dev/input/event0')
